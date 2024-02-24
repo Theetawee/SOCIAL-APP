bbfdcb782899b22061591d750a4679528d2b7df5
Forever Inc dev <redodevs@gmail.com>
2024-02-24 17:38:08 +0300

    Update constants.ts

--- hooks/constants.ts
+++ hooks/constants.ts
@@ -1,6 +1,7 @@
 
 const constants = () => {
-    const baseUrl = "http://192.168.14.40:8000";
+  // const baseUrl = "http://192.168.14.40:8000";
+  const baseUrl="https://api.waanverse.com"
     return {
       baseUrl
   }
